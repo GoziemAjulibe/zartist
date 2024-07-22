5ede4fecc2fc0befed4219add9b00671224036f6
Goziem Ajulibe <165010444+GoziemAjulibe@users.noreply.github.com>
2024-07-22 22:17:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+
+document.querySelector(".tab1").addEventListener("mouseover", function() {
+    if (!this.classList.contains("selected")) {
+        this.classList.add("hover");
+    }
+});
+
+document.querySelector(".tab1").addEventListener("mouseout", function() {
+    this.classList.remove("hover");
+});
+
+document.querySelector(".tab1").addEventListener("click", function() {
+    this.classList.add("selected");
+    document.querySelector(".tab2").classList.remove("selected");
+    document.getElementById("content1").classList.add("content-active");
+    document.getElementById("content2").classList.remove("content-active");
+});
+
+document.querySelector(".tab2").addEventListener("mouseover", function() {
+    if (!this.classList.contains("selected")) {
+        this.classList.add("hover");
+    }
+});
+
+document.querySelector(".tab2").addEventListener("mouseout", function() {
+    this.classList.remove("hover");
+});
+
+document.querySelector(".tab2").addEventListener("click", function() {
+    this.classList.add("selected");
+    document.querySelector(".tab1").classList.remove("selected");
+    document.getElementById("content2").classList.add("content-active");
+    document.getElementById("content1").classList.remove("content-active");
+});
+
+
