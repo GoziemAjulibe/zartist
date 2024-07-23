c9bb6ca6a2270848c710e75dd8e6ace397bc6f22
Goziem Ajulibe <165010444+GoziemAjulibe@users.noreply.github.com>
2024-07-23 08:45:28 +0100

    An update to remove the hover effect currently showing on mobile devices.

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ document.querySelector(".tab1").addEventListener("click", function() {
     this.classList.add("selected");
     document.querySelector(".tab2").classList.remove("selected");
     document.getElementById("content1").classList.add("content-active");
-    document.getElementById("content2").classList.remove("content-active");
 });
 
 document.querySelector(".tab2").addEventListener("mouseover", function() {
